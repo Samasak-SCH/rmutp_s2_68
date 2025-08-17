188354de9504ffe4151b966be6ba95f81f00c7b6
Samasak-SCH <somsak-tu@rmutp.ac.th>
2025-08-17 18:11:50 +0700

    Edit c.text

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ import {Hono} from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello world"));
+app.get("/", (c) => c.text("Hello world.....My name's Somsak Tubthongdee"));
 
 export default app;
