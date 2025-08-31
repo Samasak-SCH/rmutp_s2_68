3679169b9cb9219da81f54b37a0f7ce00db7968e
Samasak-SCH <somsak-tu@rmutp.ac.th>
2025-08-31 19:10:20 +0700

    Specific work name and submit date action on the same line

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,7 @@ import {Hono} from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello world.....My name's Somsak Tubthongdee"));
-app.get("/", (c) => c.text("Lab 4 on 31/08/2568"));
+app.get("/", (c) => c.text("Hello world.....My name's Somsak Tubthongdee --> Lab 4 on 31/08/2568"));
 app.get("/Profile", (c) => c.text("Profile"));
 
 export default app;
