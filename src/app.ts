85194aaa2b316eb2c2733ea505565f003341aeb8
Samasak-SCH <somsak-tu@rmutp.ac.th>
2025-08-31 23:18:27 +0700

    Import PrismaClient

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import {Hono} from "hono";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello world.....My name's Somsak Tubthongdee --> Lab 4 on 31/08/2568"));
-app.get("/Profile", (c) => c.text("Profile"));
+
+//Get data from db
+const profiles = prisma.profile.findMany();
+
+//Response
+app.get("/Profile", (c) => {
+    return c.json({
+        message:"get data completed",
+        data: profiles
+    },200);
+});
 
 export default app;
