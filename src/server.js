9a99c5acb6704f24eadbe73dc03d1054cb5ee58a
Samasak-SCH <somsak-tu@rmutp.ac.th>
2025-08-31 15:03:52 +0700

    New coding for edit port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var node_server_1 = require("@hono/node-server");
-var app_1 = require("./app");
-(0, node_server_1.serve)(app_1.default, function (info) {
-    console.log("Running server on port ".concat(info.port));
+var app_js_1 = require("./app.js");
+var desiredPort = 3001;
+(0, node_server_1.serve)({
+    fetch: app_js_1.default.fetch,
+    port: desiredPort
+}, function (info) {
+    console.log("Server is running on port ".concat(info.port));
 });
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import { serve } from "@hono/node-server";
 import app from "./app.js";
 
-// Define the port number you want to use.
-const desiredPort = 8089;
+const desiredPort = 3001;
 
-// Pass the configuration object directly as the first argument to `serve()`.
 serve({
     fetch: app.fetch,
     port: desiredPort
 }, (info) => {
-    // This callback function runs when the server successfully starts.
     console.log(`Server is running on port ${info.port}`);
 });
