084ec0724530c4e0ecd082aa522bfe69aeeef633
Samasak-SCH <somsak-tu@rmutp.ac.th>
2025-08-31 15:45:13 +0700

    --> New coding server.ts --> net stop winnat --> net start winnat

--- src/server.js
+++ src/server.js
@@ -2,10 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var node_server_1 = require("@hono/node-server");
 var app_js_1 = require("./app.js");
-var desiredPort = 3001;
-(0, node_server_1.serve)({
-    fetch: app_js_1.default.fetch,
-    port: desiredPort
-}, function (info) {
+(0, node_server_1.serve)(app_js_1.default, function (info) {
     console.log("Server is running on port ".concat(info.port));
 });
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,6 @@
 import { serve } from "@hono/node-server";
 import app from "./app.js";
 
-const desiredPort = 3001;
-
-serve({
-    fetch: app.fetch,
-    port: desiredPort
-}, (info) => {
+serve(app, (info) => {
     console.log(`Server is running on port ${info.port}`);
 });
